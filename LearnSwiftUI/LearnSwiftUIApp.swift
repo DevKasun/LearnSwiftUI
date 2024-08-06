e7338dea6545898676c6a808cd9e2e599b763b66
Kasun Lakshitha <kasun2014@outlook.com>
2024-08-06 23:49:06 +0530

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Kasun Lakshitha on 2024-08-06.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
