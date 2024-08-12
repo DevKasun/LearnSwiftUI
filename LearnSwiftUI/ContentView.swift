481403484d2afbd90bb97e6c3e23edaaf6f87c57
Kasun Lakshitha <kasun2014@outlook.com>
2024-08-13 00:33:29 +0530

    Revert "Basic structure and Navigation between screens"

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -10,13 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            HStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-            }
-            Button("Button") {}
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
         .padding()
     }
--- LearnSwiftUI/LearnSwiftUIApp.swift
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct LearnSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
             ContentView()
         }
     }
