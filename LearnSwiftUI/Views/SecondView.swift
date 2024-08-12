7b3716bb08da2750a40fcc9afa308e146ce42adc
Kasun Lakshitha <kasun2014@outlook.com>
2024-08-12 23:58:45 +0530

    Basic structure and Navigation between screens

---
+++ LearnSwiftUI/Views/SecondView.swift
@@ -0,0 +1,30 @@
+//
+//  SecondView.swift
+//  LearnSwiftUI
+//
+//  Created by Kasun Lakshitha on 2024-08-12.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        VStack {
+            Text("Second page")
+                .font(.title)
+            ScrollView {
+                HStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SecondView()
+}
